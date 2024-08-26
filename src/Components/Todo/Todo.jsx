5cebb9114da1c0ed08e4f10f6179281788200214
dreamland-minhaj <dreamland.minhaj@gmail.com>
2024-08-26 15:27:21 +0600

    todo css file modified for responsive

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -22,10 +22,10 @@ const Todo = ()=>{
         <>
         <div className="h-screen bg-slate-200 pl-10 pt-10">
             <h1 className="font-sans font-extrabold mb-3">Create ToDo List</h1>
-            <div className="max-w-96">
+            <div className="max-w-94">
                 <div className="input-area">
                     <input className="border-2 border-gray-600 rounded-md w-80" placeholder="Name" type="text" value={text} onChange={handleInputText} />
-                    <button className="bg-blue-500 text-white px-3 rounded-md font-bold ml-1 hover:bg-blue-900 focus:ring-2" onClick={handleInputBtn}>Add</button>
+                    <button className="bg-blue-500 text-white px-3 rounded-md font-bold ml-1 hover:bg-blue-900 focus:ring-2 mt-2" onClick={handleInputBtn}>Add</button>
                 </div>
                 <ul className="mt-2">
                     {todo.map((item)=>{
